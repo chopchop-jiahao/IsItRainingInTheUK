01c8fffba3032ce7cb960be7b7250285513df811
Jiahao <chopchop.jiahao@gmail.com>
2025-11-30 19:09:07 +0000

    Test WeatherCondition.from when id is in groupxx, returns drizzle

--- IsItRainingInTheUKTests/WeatherConditionTests.swift
+++ IsItRainingInTheUKTests/WeatherConditionTests.swift
@@ -28,7 +28,18 @@ enum WeatherCondition {
     case clouds
     
     static func from(id: Int) -> WeatherCondition {
+        if group2xx.contains(id) { return .thunderstorm }
+        if group3xx.contains(id) { return .drizzle }
+        
         return thunderstorm
+    }
+    
+    private static var group2xx: [Int] {
+        [200, 201, 202, 210, 211, 212, 221, 230, 231, 232]
+    }
+    
+    private static var group3xx: [Int] {
+        [300, 301, 302, 310, 311, 312, 313, 314, 321]
     }
 }
 
@@ -39,6 +50,14 @@ final class WeatherConditionTests: XCTestCase {
         
         group2xx.forEach {
             XCTAssertEqual(.thunderstorm, WeatherCondition.from(id: $0))
+        }
+    }
+    
+    func test_fromID_whenIDsInGroup3xx_returnsDrizzle() {
+        let group3xx: [Int] = [300, 301, 302, 310, 311, 312, 313, 314, 321]
+        
+        group3xx.forEach {
+            XCTAssertEqual(.drizzle, WeatherCondition.from(id: $0))
         }
     }
 }
