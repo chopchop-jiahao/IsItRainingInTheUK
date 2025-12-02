9f1925e1aac13edcbabc1ed87fba5d5765cfbda7
Jiahao <chopchop.jiahao@gmail.com>
2025-12-02 20:26:22 +0000

    Create helper method to simplify test code

--- OpenWeatherMapEndToEndTests/OpenWeatherMapEndToEndTests.swift
+++ OpenWeatherMapEndToEndTests/OpenWeatherMapEndToEndTests.swift
@@ -36,26 +36,11 @@ final class OpenWeatherMapEndToEndTests: XCTestCase {
     let baseURL = "https://api.openweathermap.org/data/3.0/onecall"
     let cheltenham = Location(latitude: 50.90, longtitude: -2.06)
     let imageURL = "https://openweathermap.org/img/wn/%@@2x.png"
+    let session = URLSession.shared
     
     func test_openWeatherMapAPI_returnsWeatherMapData() async {
-        let session = URLSession.shared
-        let url = URL(string: baseURL)!.appending(queryItems: [
-            URLQueryItem(name: "lat", value: "\(cheltenham.latitude)"),
-            URLQueryItem(name: "lon", value: "\(cheltenham.longtitude)"),
-            URLQueryItem(name: "exclude", value: "minutely,daily,alerts"),
-            URLQueryItem(name: "units", value: "metric"),
-            URLQueryItem(name: "appid", value: OpenWeatherMapAPI.key)
-        ])
-        let request = URLRequest(url: url)
-        
         do {
-            let (data, response) = try await session.data(for: request)
-            
-            let decoder = JSONDecoder()
-            decoder.dateDecodingStrategy = .secondsSince1970
-            
-            let weatherMapData = try decoder.decode(OpenWeatherMapData.self, from: data)
-            let httpResponse = response as! HTTPURLResponse
+            let (httpResponse, weatherMapData) = try await getWeatherMapData(from: makeOpenWeatherMapURL())
             
             XCTAssertEqual(httpResponse.statusCode, 200, "expected a 200 response, got \(httpResponse.statusCode) instead")
             XCTAssertEqual(weatherMapData.hourly.count, 48, "Expected 48 hourly forecast data, but got \(weatherMapData.hourly.count) instead")
@@ -83,5 +68,28 @@ final class OpenWeatherMapEndToEndTests: XCTestCase {
         } catch {
             XCTFail("Expected to succeed, but instead got error: \(error)")
         }
+    }
+    
+    private func getWeatherMapData(from url: URL, file: StaticString = #file, line: UInt = #line) async throws -> (HTTPURLResponse, OpenWeatherMapData) {
+        let request = URLRequest(url: url)
+        
+        let (data, response) = try await session.data(for: request)
+        
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .secondsSince1970
+        
+        let weatherMapData = try decoder.decode(OpenWeatherMapData.self, from: data)
+        
+        return (response as! HTTPURLResponse, weatherMapData)
+    }
+    
+    private func makeOpenWeatherMapURL() -> URL {
+        return URL(string: baseURL)!.appending(queryItems: [
+            URLQueryItem(name: "lat", value: "\(cheltenham.latitude)"),
+            URLQueryItem(name: "lon", value: "\(cheltenham.longtitude)"),
+            URLQueryItem(name: "exclude", value: "minutely,daily,alerts"),
+            URLQueryItem(name: "units", value: "metric"),
+            URLQueryItem(name: "appid", value: OpenWeatherMapAPI.key)
+        ])
     }
 }
