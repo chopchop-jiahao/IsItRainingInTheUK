42cf8916894db5251d285247230cf97c83369466
Jiahao <chopchop.jiahao@gmail.com>
2025-12-08 22:37:35 +0000

    Removed unused init

--- IsItRainingInTheUK/OpenWeatherMapAPI/OpenWeatherMapData.swift
+++ IsItRainingInTheUK/OpenWeatherMapAPI/OpenWeatherMapData.swift
@@ -10,23 +10,12 @@ import Foundation
 public struct OpenWeatherMapData: Codable, Equatable {
     public let current: WeatherData
     public let hourly: [WeatherData]
-
-    public init(current: WeatherData, hourly: [WeatherData]) {
-        self.current = current
-        self.hourly = hourly
-    }
 }
 
 public struct WeatherData: Codable, Equatable {
     public let dt: Date
     public let temp: Double
     public let weather: [WeatherDescriptionData]
-
-    public init(dt: Date, temp: Double, weather: [WeatherDescriptionData]) {
-        self.dt = dt
-        self.temp = temp
-        self.weather = weather
-    }
 }
 
 public struct WeatherDescriptionData: Codable, Equatable {
@@ -34,11 +23,4 @@ public struct WeatherDescriptionData: Codable, Equatable {
     public let main: String
     public let description: String
     public let icon: String
-
-    public init(id: Int, main: String, description: String, icon: String) {
-        self.id = id
-        self.main = main
-        self.description = description
-        self.icon = icon
-    }
 }
