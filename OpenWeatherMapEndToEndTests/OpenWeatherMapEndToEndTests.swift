e9032a6636f65c4b418652adbd4e08242f3c1eef
Jiahao <chopchop.jiahao@gmail.com>
2025-12-08 16:50:32 +0000

    format and lint the files

--- OpenWeatherMapEndToEndTests/OpenWeatherMapEndToEndTests.swift
+++ OpenWeatherMapEndToEndTests/OpenWeatherMapEndToEndTests.swift
@@ -5,19 +5,19 @@
 //  Created by Jiahao on 01/12/2025.
 //
 
-import XCTest
 import IsItRainingInTheUK
+import XCTest
 
 final class OpenWeatherMapEndToEndTests: XCTestCase {
     let session = URLSession.shared
-    
+
     func test_openWeatherMapAPI_returnsWeatherMapData() async {
         do {
             let (httpResponse, weatherMapData) = try await getWeatherMapData(from: makeOpenWeatherMapURL())
-            
+
             XCTAssertEqual(httpResponse.statusCode, 200, "expected a 200 response, got \(httpResponse.statusCode) instead")
             XCTAssertEqual(weatherMapData.hourly.count, 48, "Expected 48 hourly forecast data, but got \(weatherMapData.hourly.count) instead")
-            
+
             let currentWeatherData = weatherMapData.current
             XCTAssertNotNil(currentWeatherData, "Expected current weather data, got nil instead")
             XCTAssertNotNil(currentWeatherData.dt, "Expected current weather data to contain a valid timestamp, got nil instead")
@@ -27,36 +27,36 @@ final class OpenWeatherMapEndToEndTests: XCTestCase {
             XCTFail("Expected the request to succeed, but it failed with error: \(error)")
         }
     }
-    
+
     func test_imageRequest_returnsImageData() async {
         do {
             let (data, response) = try await session.data(for: URLRequest(url: makeImageRequestUrl()))
             let httPResponse = response as! HTTPURLResponse
-            
+
             XCTAssertEqual(httPResponse.statusCode, 200)
             XCTAssertNotNil(NSImage(data: data), "Expected valid image data")
         } catch {
             XCTFail("Expected to succeed, but instead got error: \(error)")
         }
     }
-    
+
     // MARK: - Helpers
-    
+
     private func getWeatherMapData(from url: URL, file: StaticString = #file, line: UInt = #line) async throws -> (HTTPURLResponse, OpenWeatherMapData) {
         let request = URLRequest(url: url)
-        
+
         let (data, response) = try await session.data(for: request)
-        
+
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .secondsSince1970
-        
+
         let weatherMapData = try decoder.decode(OpenWeatherMapData.self, from: data)
-        
+
         return (response as! HTTPURLResponse, weatherMapData)
     }
-    
+
     private func makeOpenWeatherMapURL() -> URL {
-        return URL(string: OpenWeatherMapAPI.baseURL)!.appending(queryItems: [
+        URL(string: OpenWeatherMapAPI.baseURL)!.appending(queryItems: [
             URLQueryItem(name: "lat", value: "\(cheltenham.latitude)"),
             URLQueryItem(name: "lon", value: "\(cheltenham.longtitude)"),
             URLQueryItem(name: "exclude", value: "minutely,daily,alerts"),
@@ -64,13 +64,13 @@ final class OpenWeatherMapEndToEndTests: XCTestCase {
             URLQueryItem(name: "appid", value: OpenWeatherMapAPI.key)
         ])
     }
-    
+
     private func makeImageRequestUrl() -> URL {
         let clearSkyIcon = "01d"
-        
+
         return URL(string: String(format: OpenWeatherMapAPI.imageURL, clearSkyIcon))!
     }
-    
+
     private var cheltenham: Location {
         Location(latitude: 50.90, longtitude: -2.06)
     }
