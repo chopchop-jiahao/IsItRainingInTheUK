4c9eda5f4667e2f2dd66ee4aabc73a8c1c15462f
Jiahao <chopchop.jiahao@gmail.com>
2025-12-09 07:37:55 +0000

    Create a helper file test-shared scpe helper functions and properties

--- IsItRainingInTheUKTests/WeatherCacheTests.swift
+++ IsItRainingInTheUKTests/WeatherCacheTests.swift
@@ -11,25 +11,41 @@ import IsItRainingInTheUK
 // test get to return nil if the caches expires
 // test set updates the cache
 class WeatherStore: WeatherCache {
+    private var cache = [URL : OpenWeatherMapData]()
+    
     func get(for url: URL) -> OpenWeatherMapData? {
-        return nil
+        return cache[url]
     }
     
     func set(_ data: OpenWeatherMapData, for url: URL) {
-        
+        cache[url] = data
     }
 }
 
 final class WeatherCacheTests: XCTestCase {
     
     func test_get_returnsNil_whenNoDataStored() {
-        let sut = WeatherStore()
+        let sut =  makeSUT()
+        let url = anyURL
         
-        let data = sut.get(for: anyURL)
+        let data = sut.get(for: url)
         
         XCTAssertNil(data)
     }
     
+    func test_get_returnsStoredData_whenDataIsStored() throws {
+        let sut = makeSUT()
+        let data = openWeatherMapJsonData()
+        let url = anyURL
+        
+        try sut.set(openWeatherMapData(from: data), for: url)
+        
+        XCTAssertNotNil(sut.get(for: url))
+    }
+    
+    private func makeSUT() -> WeatherCache {
+        WeatherStore()
+    }
     
     private var anyURL: URL {
         URL(string: "any-url")!
