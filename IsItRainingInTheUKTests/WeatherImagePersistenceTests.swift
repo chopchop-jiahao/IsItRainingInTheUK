c603bda6af91f986fdbcca0d04033246baa11b97
Jiahao <chopchop.jiahao@gmail.com>
2025-12-14 14:58:52 +0000

    Test save stores data to given url

--- IsItRainingInTheUKTests/WeatherImagePersistenceTests.swift
+++ IsItRainingInTheUKTests/WeatherImagePersistenceTests.swift
@@ -9,18 +9,40 @@ import XCTest
 import IsItRainingInTheUK
 
 class WeatherImageStore: WeatherImagePersistence {
-    func find(imageWithCode: String) async -> Data? {
-        return .none
+    private let fileManager: FileManager
+    private let storeURL: URL
+    
+    init(fileManager: FileManager = .default, storeURL: URL) {
+        self.fileManager = fileManager
+        self.storeURL = storeURL
+    }
+    
+    func find(imageWithCode code: String) async -> Data? {
+        let fileURL = storeURL.appendingPathComponent(code)
+        
+        return try? Data(contentsOf: fileURL)
     }
     
     func save(imageData: Data, for code: String) {
+        let fileURL = storeURL.appendingPathComponent(code)
         
+        try? fileManager.createDirectory(at: storeURL, withIntermediateDirectories: true)
+        
+        try? imageData.write(to: fileURL)
     }
 }
 
 final class WeatherImagePersistenceTests: XCTestCase {
     
-    func test_find_deliversNoData_whenNoImageStored() async throws {
+    override func setUp() {
+        clearStoreArtefacts()
+    }
+    
+    override func tearDown() {
+        clearStoreArtefacts()
+    }
+    
+    func test_find_deliversNoData_whenNoImageStoredForCode() async throws {
         let sut = makeSUT()
         let code = "code"
         
@@ -29,7 +51,31 @@ final class WeatherImagePersistenceTests: XCTestCase {
         XCTAssertNil(data)
     }
     
+    func test_save_storesDataToStore() async throws {
+        let sut = makeSUT()
+        let code = "code"
+        let expectedData = anyData()
+        
+        sut.save(imageData: expectedData, for: code)
+        
+        let retrievedData = await sut.find(imageWithCode: code)
+        
+        XCTAssertEqual(expectedData, retrievedData, "Expected to retrieve \(expectedData), but got \(String(describing: retrievedData)) instead")
+    }
+    
     private func makeSUT() -> WeatherImagePersistence {
-        WeatherImageStore()
+        WeatherImageStore(storeURL: testURL)
+    }
+    
+    private func anyData() -> Data {
+        Data("image".utf8)
+    }
+    
+    private var testURL: URL {
+        FileManager.default.temporaryDirectory.appendingPathComponent("\(WeatherImagePersistenceTests.self)")
+    }
+    
+    private func clearStoreArtefacts() {
+        try? FileManager.default.removeItem(at: testURL)
     }
 }
