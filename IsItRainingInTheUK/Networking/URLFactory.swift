693c3f56f61a1892e630cc55681e9f2a1110769f
Jiahao <chopchop.jiahao@gmail.com>
2025-12-27 16:42:30 +0000

    Make URLFactory internal

--- IsItRainingInTheUK/Networking/URLFactory.swift
+++ IsItRainingInTheUK/Networking/URLFactory.swift
@@ -11,8 +11,8 @@ enum URLFactoryError: Error {
     case invalidURL
 }
 
-public enum URLFactory {
-    public static func getURL(for location: Location) throws -> URL {
+enum URLFactory {
+    static func getURL(for location: Location) throws -> URL {
         guard let baseURL = URL(string: OpenWeatherMapAPI.baseURL) else {
             throw URLFactoryError.invalidURL
         }
@@ -26,7 +26,7 @@ public enum URLFactory {
         ])
     }
 
-    public static func getImageRequestUrl(withCode code: String) throws -> URL {
+    static func getImageRequestUrl(withCode code: String) throws -> URL {
         guard let url = URL(string: String(format: OpenWeatherMapAPI.imageURL, code)) else {
             throw URLFactoryError.invalidURL
         }
